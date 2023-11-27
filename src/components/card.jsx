2f1ec3be0521b5fe389ee9aa0037d8a53eed7866
Anuj <anuj@localhost.localdomain>
2023-11-28 00:02:55 +0545

    added card component

---
+++ src/components/card.jsx
@@ -0,0 +1,16 @@
+function Card() {
+    return (
+        <>
+            <div style={{display:'grid', gap:'10px'}}>
+                <div>
+                    <img src="" alt="image1" />
+                </div>
+                <div>
+                    <p>Nice</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Card;
